99504ea18f55614ee5b4cf0a8196169f450ddc2b
Adonis Ling <adonis0147@gmail.com>
2016-09-27 18:04:59 +0800

    Add CMakeLists.txt

--- cIndexer/cIndexer.h
+++ cIndexer/cIndexer.h
@@ -1,6 +1,22 @@
+#pragma once
+#ifndef CINDEXER_H
+#define CINDEXER_H
+
+#ifdef _DEBUG
+#undef _DEBUG
+#define _DEBUG_WAS_DEFINED
+#endif
+
 #include <Python.h>
 #include <vector>
 #include <string>
 
 static std::vector<std::string> Scan(const std::string &root, const std::string &pattern = "^$");
 static PyObject *Scan(PyObject *self, PyObject *args);
+
+#ifdef _DEBUG_WAS_DEFINED
+#define _DEBUG
+#endif
+
+#endif
+
