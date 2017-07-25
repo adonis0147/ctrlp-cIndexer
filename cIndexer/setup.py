0ab99d8abc29a66d4b70c9bd2eff5246fdec3941
Adonis Ling <adonis0147@gmail.com>
2017-07-25 09:00:41 +0800

    Update setup.py

--- cIndexer/setup.py
+++ cIndexer/setup.py
@@ -1,10 +1,16 @@
+import sys
 from distutils.core import setup, Extension
 
-module = Extension('cIndexer', sources = ['cIndexer.cc'],
-                   extra_compile_args = ['--std=c++11', '-Wno-write-strings'])
+if sys.platform != 'win32':
+    extra_compile_args = ['--std=c++11', '-Wno-write-strings']
+else:
+    extra_compile_args = []
 
-setup(name = 'cIndexer',
-      version = '0.1',
-      description = 'Indexer for ctrlp',
-      ext_modules = [module])
+module = Extension('cIndexer', sources=['cIndexer.cc'],
+                   extra_compile_args=extra_compile_args)
+
+setup(name='cIndexer',
+      version='0.1',
+      description='Indexer for ctrlp',
+      ext_modules=[module])
 
