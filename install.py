f916e6c4358b179fed09f9e84bcd67bcc679ff38
Adonis Ling <adonis0147@gmail.com>
2025-05-13 22:32:42 +0800

    Fix build errors

--- install.py
+++ install.py
@@ -6,7 +6,7 @@ import glob
 import shutil
 
 path = os.environ.get("PATH").replace(os.environ.get("HOME") + '/.rye/shims', '')
-os.system('PATH=%s cd cIndexer && python3 setup.py build' % path)
+os.system("cd cIndexer && PATH='%s' python3 setup.py build" % path)
 
 extensions = ['*.so', '*.pyd', '*.dll']
 path = './cIndexer/build/*'
